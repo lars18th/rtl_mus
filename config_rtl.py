92a1586db7d265c8fede781835ae72730ac888e4
lars18th <lars18th@users.noreply.github.com>
2018-02-25 21:00:17 +0100

    Add config values for the fake dongle mode

--- config_rtl.py
+++ config_rtl.py
@@ -27,8 +27,8 @@ send_first=""
 #            + chr(0x00) + chr(0x00) + chr(0x00) + chr(0x05)
 #            ) # if you like to rewrite the DONGLE-ID. Required when using fake dongle.
 dongle_id=""
-rtl_tcp_host = 'localhost'
-rtl_tcp_port = 1234
+rtl_tcp_host = 'localhost'	# set to "" (NULL) when you use the fake dongle.
+rtl_tcp_port = 1234		# when you set to 0 you're enabling the fake dongle!
 
 setuid_on_start = 0	# we normally start with root privileges and setuid() to another user
 uid = 999		# determine by issuing: $ id -u username
@@ -64,8 +64,37 @@ allowed_ip_ranges=()
 allow_gain_set=1
 
 use_dsp_command=False	# you can process raw I/Q data with a custom command that starts a process that we can pipe the data into, and also pipe out of.
+			# when using the fake dongle mode it's mandatory to enable the DSP
 debug_dsp_command=False	# show sample rate before and after the dsp command
-dsp_command=""
+dsp_command=""			# when using the fake dongle mode this it's required
+dsp_chunk_size=1024		# use values like '16348','1024','16384'. It's the size of READ chunks from the DSP process. Recomendation: use the same value as the command in the DSP process  
+dsp_sampling_rate=250000	# when using the fake dongle it's required to resynch
+dsp_resynch_loop=1		# number of reads when resynching using the fake dongle
+'''
+Example of fake dongle mode: reading samples from a FIFO file
+
+# Read from fifo "/raw-fifo" in chunks of 16384 bytes
+dsp_command="dd if=/raw-fifo bs=16384 status=noxfer"
+
+# Set the sampling rate for regular 2048000 Msps
+dsp_sampling_rate=2048000
+
+# Do a resynch every 8 loops (try with 8,16,32 values) 
+dsp_resynch_loop=8
+
+Reset client values
+rtl_tcp_host=""
+rtl_tcp_port=0 # mandatory for fake dongle
+use_dsp_command=True # mandatory for fake dongle
+debug_dsp_command=True # recomended
+watchdog_interval=0 # disable it
+
+Set the Dongle-ID
+dongle_id = ( chr(0x52) + chr(0x54) + chr(0x4c) + chr(0x30)
+            + chr(0x00) + chr(0x00) + chr(0x00) + chr(0x02)
+            + chr(0x00) + chr(0x00) + chr(0x00) + chr(0x05)
+            ) # example for simulating a generic RTL2832U
+'''
 
 '''
 Example DSP commands:
